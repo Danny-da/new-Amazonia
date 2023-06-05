910f4d699d377b42b294de971cc0fd548f5b042f
Danny-da <896547@senati.pe>
2023-06-05 12:45:58 -0500

    Amazonia Actualizada

--- src/components/body.js
+++ src/components/body.js
@@ -1,122 +1,153 @@
 import React from 'react';
 import './estilo.css';
-import { Card, CardContent, Grid, CardMedia, Typography, Fab, Box, CardActionArea } from '@mui/material';
+import { Card, CardContent, Grid, CardMedia, Typography, Fab, Box } from '@mui/material';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import AutoAwesomeOutlinedIcon from '@mui/icons-material/AutoAwesomeOutlined';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import img1 from '../images/card.png'
 import img2 from '../images/card2.png'
 import img3 from '../images/card 3.png'
 import map from '../images/mapa.png'
+import amazo from '../images/amazo.png'
 
 const Body = () => {
   return (
-    <div style={{ padding: '50px' }}>
-      <h2 style={{ textAlign: 'center' }}>Pueblos Indígenas</h2>
-      <h1 style={{ textAlign: 'center' }}>Descubriendo las raíces indígenas</h1>
-      <br />
-
-      <Card className="card" style={{ boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.1)' }}>
-        <Grid container>
-          <Grid item xs={12} sm={4}>
-            <img src={img1} alt="Imagen 1" />
-          </Grid>
-          <Grid item xs={12} sm={8}>
-            <CardContent>
-              <Typography variant="h5" component="div" style={{ marginLeft: '10px', fontWeight: 'bold' }}>
-                ¿Conoces a la etnia de los Achuars?
-              </Typography>
-              <Typography variant="body2" color="text.secondary" style={{ marginLeft: '10px' }}>
-                El pueblo achuar vive principalmente en el norte del departamento de Loreto, cerca de la frontera con Ecuador. Según el censo de...
-              </Typography>
-              <Box mt={2} style={{ marginLeft: '65%' }}>
-                <Fab variant="extended" size="medium" color="primary">
-                  Leer más
-                </Fab>
-              </Box>
-            </CardContent>
-          </Grid>
-        </Grid>
-      </Card>
-
-      <Card className="card" style={{ boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.1)' }}>
-        <Grid container>
-          <Grid item xs={12} sm={4}>
-            <img src={img2} alt="Imagen 2" />
-          </Grid>
-          <Grid item xs={12} sm={8}>
-            <CardContent>
-              <Typography variant="h5" component="div" style={{ marginLeft: '10px', fontWeight: 'bold' }}>
-                ¿Conoces la etnia de los Boras?
-              </Typography>
-              <Typography variant="body2" color="text.secondary" style={{ marginLeft: '10px' }}>
-                El pueblo bora se ubica en el departamento de Loreto, en el territorio bañado por los ríos Ampiyacu y Yahuasyacu, cerca de la ciudad...
-              </Typography>
-              <Box mt={2} style={{ marginLeft: '65%' }}>
-                <Fab variant="extended" size="medium" color="primary">
-                  Leer más
-                </Fab>
-              </Box>
-            </CardContent>
-          </Grid>
-        </Grid>
-      </Card>
-
-      <Card className="card" style={{ boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.1)' }}>
-        <Grid container>
-          <Grid item xs={12} sm={4}>
-            <img src={img3} alt="Imagen 3" />
-          </Grid>
-          <Grid item xs={12} sm={8}>
-            <CardContent>
-              <Typography variant="h5" component="div" style={{ marginLeft: '10px', fontWeight: 'bold' }}>
-                ¿Conoces la etnia de los Ticunas?
-              </Typography>
-              <Typography variant="body2" color="text.secondary" style={{ marginLeft: '10px' }}>
-                El pueblo ticuna se ha asentado en ambas márgenes del río Amazonas y sus afluentes, cerca de la frontera peruana con Colombia...
-              </Typography>
-              <Box mt={2} className="button-container">
-                <Fab variant="extended" size="medium" color="primary">
-                  Leer más
-                </Fab>
-              </Box>
-            </CardContent>
-          </Grid>
-        </Grid>
-      </Card>
-
-      <img src={map} alt="Logo de Amazonía" style={{ width: '68%', marginLeft: "45%", marginTop: '-100%' }} />
-      <Box mt={2} style={{ textAlign: 'center' }}>
-        <Fab variant="extended" color="primary" aria-label="show more">
-          Ver más
-          <ChevronRightIcon fontSize="medium" style={{ marginLeft: '5px' }} />
-        </Fab>
-      </Box>
-
-      <div style={{ padding: '50px' }}>
-        <h2 style={{ textAlign: 'center' }}>Especies</h2>
-        <h1 style={{ textAlign: 'center' }}>Explora la asombrosa variedad de flora y fauna en la región amazónica</h1>
-        <br />
-        {/* Contenido del segundo div */}
-      </div>
-
-      <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        {/* <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        /> */}
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
+    <div>
+      <div style={{marginTop:'-10%'}}>
+      <img src={amazo} alt="Logo de Amazonía" style={{ width: '40%', marginLeft: "10%", marginTop: '20%' }} />
+      
+      <Card className="ca-rd" style={{ boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.1)' }}>
+        <CardContent style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+          <Typography variant="h5" component="div" style={{ display: 'flex', alignItems: 'center' }}>
+            <Box mt={2} style={{ marginLeft: '1%' }}>
+              <AutoAwesomeOutlinedIcon fontSize="large" style={{ marginRight: '15px' }} />
+            </Box>
+            Diversidad Impactante en la Amazonía
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+          <Typography variant="body2" color="text.secondary" style={{ flexGrow: 1, marginLeft: '57px' }}>
+            La guía ilustrada del Instituto de Investigaciones de la Amazonía Peruana muestra la increíble diversidad de especies de flora y fauna en la región. Es una herramienta valiosa para la formación y sensibilización de jóvenes y adultos, tanto dentro como fuera de la Amazonía, debido a su belleza y relevancia impactantes.
+          </Typography>
+
+        </CardContent>
+      </Card>
+
+      <Card className="ca-rd" style={{ boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.1)' }}>
+        <CardContent style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+          <Typography variant="h5" component="div" style={{ display: 'flex', alignItems: 'center' }}>
+            <Box mt={2} style={{ marginLeft: '1%' }}>
+              <CheckCircleOutlineIcon fontSize="large" style={{ marginRight: '15px' }} />
+            </Box>
+            Nuestro objetivo
+          </Typography>
+          <Typography variant="body2" color="text.secondary" style={{ flexGrow: 1, marginLeft: '57px' }}>
+            Nuestro objetivo es dar a conocer la importancia de la
+            diversidad biológica y fomentar su protección y respeto.
+            Reconocemos que Ia degradación y el cambio climático
+            afectan a todos, pero especialmente a las regiones más
+            pobres y a las poblaciones vulnerables que dependen de
+            los recursos naturales para sobrevivir.
           </Typography>
         </CardContent>
-      </CardActionArea>
-    </Card>
+      </Card>
+      </div>
+
+      {/* <Card className="ca-rd" style={{ boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.1)' }}>
+          <Grid container>
+            <Grid item xs={12} sm={8}>
+              <CardContent style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%'  }}>
+                <Typography variant="h5" component="div" style={{ marginLeft: '70px', fontWeight: 'bold',  flexGrow: 1, width: '100%'  }}>
+                Diversidad Impactante en la Amazonía
+                </Typography>
+                <Typography variant="body2" color="text.secondary" style={{ marginLeft: '70px',  flexGrow: 1, width: '130%'  }}>
+                La guía ilustrada del Instituto de Investigaciones de la Amazonía Peruana muestra la increíble diversidad de especies de flora y fauna en la región. Es una herramienta valiosa para la formación y sensibilización de jóvenes y adultos, tanto dentro como fuera de la Amazonía, debido a su belleza y relevancia impactantes.
+                </Typography>
+              </CardContent>
+            </Grid>
+          </Grid>
+        </Card> */}
+
+      <div style={{ padding: '50px' }}>
+        <h2 style={{ textAlign: 'center' }}>Pueblos Indígenas</h2>
+        <h1 style={{ textAlign: 'center' }}>Descubriendo las raíces indígenas</h1>
+        <br />
+
+        <Card className="card" style={{ boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.1)' }}>
+          <Grid container>
+            <Grid item xs={12} sm={4}>
+              <img src={img1} alt="Imagen 1" />
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <CardContent>
+                <Typography variant="h5" component="div" style={{ marginLeft: '10px', fontWeight: 'bold' }}>
+                  ¿Conoces a la etnia de los Achuars?
+                </Typography>
+                <Typography variant="body2" color="text.secondary" style={{ marginLeft: '10px' }}>
+                  El pueblo achuar vive principalmente en el norte del departamento de Loreto, cerca de la frontera con Ecuador. Según el censo de...
+                </Typography>
+                <Box mt={2} style={{ marginLeft: '65%' }}>
+                  <Fab variant="extended" size="medium" color="primary">
+                    Leer más
+                  </Fab>
+                </Box>
+              </CardContent>
+            </Grid>
+          </Grid>
+        </Card>
+
+        <Card className="card" style={{ boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.1)' }}>
+          <Grid container>
+            <Grid item xs={12} sm={4}>
+              <img src={img2} alt="Imagen 2" />
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <CardContent>
+                <Typography variant="h5" component="div" style={{ marginLeft: '10px', fontWeight: 'bold' }}>
+                  ¿Conoces la etnia de los Boras?
+                </Typography>
+                <Typography variant="body2" color="text.secondary" style={{ marginLeft: '10px' }}>
+                  El pueblo bora se ubica en el departamento de Loreto, en el territorio bañado por los ríos Ampiyacu y Yahuasyacu, cerca de la ciudad...
+                </Typography>
+                <Box mt={2} style={{ marginLeft: '65%' }}>
+                  <Fab variant="extended" size="medium" color="primary">
+                    Leer más
+                  </Fab>
+                </Box>
+              </CardContent>
+            </Grid>
+          </Grid>
+        </Card>
+
+        <Card className="card" style={{ boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.1)' }}>
+          <Grid container>
+            <Grid item xs={12} sm={4}>
+              <img src={img3} alt="Imagen 3" />
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <CardContent>
+                <Typography variant="h5" component="div" style={{ marginLeft: '10px', fontWeight: 'bold' }}>
+                  ¿Conoces la etnia de los Ticunas?
+                </Typography>
+                <Typography variant="body2" color="text.secondary" style={{ marginLeft: '10px' }}>
+                  El pueblo ticuna se ha asentado en ambas márgenes del río Amazonas y sus afluentes, cerca de la frontera peruana con Colombia...
+                </Typography>
+                <Box mt={2} className="button-container">
+                  <Fab variant="extended" size="medium" color="primary">
+                    Leer más
+                  </Fab>
+                </Box>
+              </CardContent>
+            </Grid>
+          </Grid>
+        </Card>
+
+        <img src={map} alt="Logo de Amazonía" style={{ width: '68%', marginLeft: "45%", marginTop: '-100%' }} />
+        <Box mt={2} style={{ textAlign: 'center' }}>
+          <Fab variant="extended" color="primary" aria-label="show more">
+            Ver más
+            <ChevronRightIcon fontSize="medium" style={{ marginLeft: '5px' }} />
+          </Fab>
+        </Box>
+      </div>
     </div>
   );
 };
